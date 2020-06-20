3889da15a94c2c7cbfe0a3d75b3f83c380dabd16
vpoletaev11 <vpoletaev11@gmail.com>
2020-06-21 00:32:49 +0300

    Some variables now global in tests

--- search/search_test.go
+++ search/search_test.go
@@ -7,38 +7,33 @@ import (
 	"github.com/vpoletaev11/go-algorithms/search"
 )
 
-func TestLinearSearch(t *testing.T) {
-	dataset := []int{4, 7, 1, 3, 11, 82, 54, 6, 6, 14, 9, 13, 28}
+var datasetUnsorted = []int{9, 55, 55, 55, 8, 1, 9, 12, 88, 3}
+var datasetSorted = []int{0, 9, 9, 9, 10, 14, 16, 23, 25, 30}
 
-	assert.Equal(t, 10, search.LinearSearch(dataset, 9))    // success
-	assert.Equal(t, -1, search.LinearSearch(dataset, 1000)) // not found
+func TestLinearSearch(t *testing.T) {
+	assert.Equal(t, 4, search.LinearSearch(datasetUnsorted, 8))     // success
+	assert.Equal(t, -1, search.LinearSearch(datasetUnsorted, 1000)) // not found
 }
 
 func TestLinearMultipleSearch(t *testing.T) {
-	dataset := []int{4, 7, 1, 3, 11, 82, 54, 6, 6, 14, 9, 13, 28}
-
-	assert.Equal(t, []int{7, 8}, search.LinearMultipleSearch(dataset, 6)) // success
-	assert.Equal(t, []int{}, search.LinearMultipleSearch(dataset, 1000))  // not found
+	assert.Equal(t, []int{0, 6}, search.LinearMultipleSearch(datasetUnsorted, 9)) // success
+	assert.Equal(t, []int{}, search.LinearMultipleSearch(datasetUnsorted, 1000))  // not found
 }
 
 func TestBinarySearch(t *testing.T) {
-	dataset := []int{1, 2, 4, 7, 9, 13, 15, 20, 23, 28, 31, 42, 48, 50}
-
-	assert.Equal(t, 4, search.BinarySearch(dataset, 9))     // success
-	assert.Equal(t, -1, search.BinarySearch(dataset, 1000)) // not found
+	assert.Equal(t, 1, search.BinarySearch(datasetSorted, 9))     // success
+	assert.Equal(t, -1, search.BinarySearch(datasetSorted, 1000)) // not found
 }
 
 func TestBinaryMultipleSearch(t *testing.T) {
-	dataset := []int{1, 2, 3, 4, 5, 5, 5, 5, 6, 8, 15, 19, 20, 30, 48, 56, 100}
-
 	// success
-	from, to := search.BinaryMultipleSearch(dataset, 5)
+	from, to := search.BinaryMultipleSearch(datasetSorted, 9)
 	actual := []int{from, to}
-	expected := []int{4, 7}
+	expected := []int{1, 3}
 	assert.Equal(t, expected, actual)
 
 	// not found
-	from, to = search.BinaryMultipleSearch(dataset, 1000)
+	from, to = search.BinaryMultipleSearch(datasetSorted, 1000)
 	actual = []int{from, to}
 	expected = []int{-1, -1}
 	assert.Equal(t, expected, actual)
@@ -53,7 +48,7 @@ func TestBinaryMultipleSearchAvoidOutOfRange(t *testing.T) {
 	expected := []int{0, 5}
 	assert.Equal(t, expected, actual)
 
-	// not found
+	// right bound
 	from, to = search.BinaryMultipleSearch(dataset, 2)
 	actual = []int{from, to}
 	expected = []int{6, 10}
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -7,34 +7,33 @@ import (
 	"github.com/vpoletaev11/go-algorithms/sort"
 )
 
+var expectedOut = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
 func TestBubbleSort(t *testing.T) {
 	dataset := []int{10, 1, 3, 6, 5, 7, 2, 8, 9, 4}
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	sort.BubbleSort(dataset)
-	assert.Equal(t, expected, dataset)
+	assert.Equal(t, expectedOut, dataset)
 }
 
 func TestHeapSort(t *testing.T) {
 	dataset := []int{10, 1, 3, 6, 5, 7, 2, 8, 9, 4}
-	expected := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+	expectedOut := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
 
 	sort.HeapSort(dataset)
-	assert.Equal(t, expected, dataset)
+	assert.Equal(t, expectedOut, dataset)
 }
 
 func TestInsertionSort(t *testing.T) {
 	dataset := []int{10, 1, 3, 6, 5, 7, 2, 8, 9, 4}
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	sort.InsertionSort(dataset)
-	assert.Equal(t, expected, dataset)
+	assert.Equal(t, expectedOut, dataset)
 }
 
 func TestQuickSort(t *testing.T) {
 	dataset := []int{10, 1, 3, 6, 5, 7, 2, 8, 9, 4}
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	sort.QuickSort(dataset)
-	assert.Equal(t, expected, dataset)
+	assert.Equal(t, expectedOut, dataset)
 }
